72c7a769cd687d0327a335e0270313b8dbbf5d35
Manuel Amunategui <amunategui@gmail.com>
2015-04-29 18:46:09 -0700

    walkthrough to pubmed code

---
+++ pubmed-query.R
@@ -0,0 +1,23 @@
+#install.packages("RISmed")
+library(RISmed)
+
+search_topic <- 'copd'
+search_query <- EUtilsSummary(search_topic, retmax=100, mindate=2012,maxdate=2012)
+summary(search_query)
+
+# see the ids of our returned query
+QueryId(search_query)
+
+# get actual data from PubMed
+records<- EUtilsGet(search_query)
+class(records)
+
+# store it
+pubmed_data <- data.frame('Title'=ArticleTitle(records),'Abstract'=AbstractText(records))
+head(pubmed_data,1)
+
+pubmed_data$Abstract <- as.character(pubmed_data$Abstract)
+pubmed_data$Abstract <- gsub(",", " ", pubmed_data$Abstract, fixed = TRUE)
+
+# see what we have
+str(pubmed_data)
